022494a047de525c75da522d8d987c3822e33df0
tpmanc <tpxtrime@mail.ru>
2017-12-24 13:51:46 +0300

    fix assets

--- widgets/RedactorAsset.php
+++ widgets/RedactorAsset.php
@@ -14,18 +14,12 @@ namespace yii\redactor\widgets;
  */
 class RedactorAsset extends \yii\web\AssetBundle
 {
-    public $sourcePath = '@vendor/yiidoc/yii2-redactor/assets';
+    public $sourcePath = '@vendor/tpmanc/yii2-redactor/assets';
     public $depends = ['yii\web\JqueryAsset'];
 
     public function init()
     {
-        if (YII_DEBUG) {
-            $this->js[] = 'redactor.js';
-            $this->css[] = 'redactor.css';
-        } else {
-            $this->js[] = 'redactor.min.js';
-            $this->css[] = 'redactor.min.css';
-        }
+        $this->js[] = 'redactor.js';
+        $this->css[] = 'redactor.css';
     }
-
 }
